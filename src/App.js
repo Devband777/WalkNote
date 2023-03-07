1e07d060c11df5ed51715060e2ce418cdc772bd0
Devband777 <world.devband777@gmail.com>
2023-03-07 03:03:11 -0500

    Add fix

--- src/App.js
+++ src/App.js
@@ -17,15 +17,18 @@ import { useEffect, useState } from 'react';
 import './App.scss';
 
 function App(props) {
-  let [counter, setCounter] = useState(3);
+  let [counter, setCounter] = useState(0);
   
   useEffect(() => {
     window.addEventListener('keydown', e => {
+
       if(e.key === 'ArrowRight'){
-       setCounter(counter+1)
+        if(counter === 9) return;
+       setCounter(counter + 1)
       }
       if(e.key === 'ArrowLeft'){
-        setCounter(counter-1)
+        if(counter === 0) return;
+        setCounter(counter - 1)
        }
      })    
   })
@@ -34,34 +37,34 @@ function App(props) {
     <div className="App">
       <div className="container">
         {
-          counter === 1 && <First />
+          counter === 0 && <First />
         }
         {
-          counter === 2 && <Second />
+          counter === 1 && <Second />
         }
         {
-          counter === 3 && <Third />
+          counter === 2 && <Third />
         }
         {
-          counter === 4 && <Fourth />
+          counter === 3 && <Fourth />
         }
         {
-          counter === 5 && <Fifth />
+          counter === 4 && <Fifth />
         }
         {
-          counter === 6 && <Sixth />
+          counter === 5 && <Sixth />
         }
         {
-          counter === 7 && <Seventh />
+          counter === 6 && <Seventh />
         }
         {
-          counter === 8 && <Eighth />
+          counter === 7 && <Eighth />
         }
         {
-          counter === 9 && <Nineth />
+          counter === 8 && <Nineth />
         }
         {
-          counter === 10 && <Tenth />
+          counter === 9 && <Tenth />
         }
       </div>
     </div>
